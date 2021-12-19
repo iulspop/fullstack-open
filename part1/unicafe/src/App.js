03c4ea851ae109d211dc37f11f7919b5bc024119
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-12-18 20:12:26 -0500

    Complete part 1, exercise 1.9-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,12 @@ export default function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const statistics = [
+    { text: 'good', value: good },
+    { text: 'neutral', value: neutral },
+    { text: 'bad', value: bad },
+  ]
+
   const incrementGood = () => setGood(good + 1)
   const incrementNeutral = () => setNeutral(neutral + 1)
   const incrementBad = () => setBad(bad + 1)
@@ -17,30 +23,28 @@ export default function App() {
       <button onClick={incrementNeutral}>neutral</button>
       <button onClick={incrementBad}>bad</button>
       <h2>statistics</h2>
-      {good === 0 && neutral === 0 && bad === 0 ? (
-        <p>No feedback given</p>
-      ) : (
-        <Statistics good={good} neutral={neutral} bad={bad} />
-      )}
+      {good === 0 && neutral === 0 && bad === 0 ? <p>No feedback given</p> : <Statistics statistics={statistics} />}
     </div>
   )
 }
 
-function Statistics({ good, neutral, bad }) {
+function Statistics({ statistics }) {
+  const all = statistics.reduce((total, stat) => total + stat.value, 0)
+  const average = (all / 3).toFixed(2)
+  const positive = ((statistics[0].value / all) * 100).toFixed(2)
+
+  statistics = statistics.concat({ text: 'all', value: all })
+  statistics = statistics.concat({ text: 'average', value: average })
+  statistics = statistics.concat({ text: 'positive', value: String(positive) + ' %' })
+
+  return statistics.map(({ text, value }, idx) => <StatisticsLine text={text} value={value} key={idx} />)
+}
+
+function StatisticsLine({ text, value }) {
   return (
-    <>
-      <div>
-        <dt>good</dt>
-        <dd>{good}</dd>
-      </div>
-      <div>
-        <dt>neutral</dt>
-        <dd>{neutral}</dd>
-      </div>
-      <div>
-        <dt>bad</dt>
-        <dd>{bad}</dd>
-      </div>
-    </>
+    <div>
+      <dt>{text}</dt>
+      <dd>{value}</dd>
+    </div>
   )
 }
