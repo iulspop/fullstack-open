6a6897daddfd527f5b2431bc252500d639b5b60b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-12-18 19:53:29 -0500

    Complete part 1, exercise 1.6-1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={incrementGood}>good</button>
+      <button onClick={incrementNeutral}>neutral</button>
+      <button onClick={incrementBad}>bad</button>
+      <h2>statistics</h2>
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+function Statistics({ good, neutral, bad }) {
+  return (
+    <>
+      <div>
+        <dt>good</dt>
+        <dd>{good}</dd>
+      </div>
+      <div>
+        <dt>neutral</dt>
+        <dd>{neutral}</dd>
+      </div>
+      <div>
+        <dt>bad</dt>
+        <dd>{bad}</dd>
+      </div>
+    </>
+  )
+}
