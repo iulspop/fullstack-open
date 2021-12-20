e26ae2ebabd52a802ef6f7b538a87b221c2da944
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-12-20 16:31:07 -0500

    Complete part 2, exercise 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
+import './App.css'
+
+export default function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then(res => setCountries(res.data))
+  }, [])
+
+  const [countriesFilter, setCountriesFilter] = useState('')
+  const handleCountriesFilterChange = e => {
+    setCountriesFilter(e.target.value)
+  }
+
+  console.log(countries)
+  const filteredCountries = countries.filter(country => country.name.common.match(new RegExp(countriesFilter, 'i')))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label htmlFor="countriesFilter">find countries</label>
+        <input name="countriesFilter" value={countriesFilter} onChange={handleCountriesFilterChange} />
+      </form>
+      <ul>
+        {filteredCountries.length === 1 ? (
+          <div>
+            <h2>{countries[0].name.common}</h2>
+            <p>capital {countries[0].capital}</p>
+            <p>population {countries[0].population}</p>
+            <h3>languages</h3>
+            <ul>{listLanguages(countries[0])}</ul>
+            <img class="flag" src={countries[0].flags.png}/>
+          </div>
+        ) : filteredCountries.length <= 10 ? (
+          filteredCountries.map(country => <li key={country.ccn3}>{country.name.common}</li>)
+        ) : (
+          <p>Too many matches, specify another filter</p>
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+const listLanguages = country => {
+  const items = []
+  for (let language in country.languages) {
+    items.push(<li key={language}>{country.languages[language]}</li>)
+  }
+  return items
+}
