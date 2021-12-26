0ca9c4656b9c64f17d0877f3cacb4c8b54cc2a5f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-12-26 18:36:10 -0500

    Complete part 3, exercise 3.1-3.16

---
+++ part3/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const fs = require('fs')
+const uuid = require('uuid')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/persons', (_, res) => {
+  const persons = JSON.parse(fs.readFileSync('./db.json', 'utf-8'))
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const persons = JSON.parse(fs.readFileSync('./db.json', 'utf-8'))
+
+  const person = persons.find(person => person.id === req.params.id)
+  if (person === undefined) {
+    res.sendStatus(404)
+  } else {
+    res.json(person)
+  }
+})
+
+app.post('/api/persons', (req, res) => {
+  const persons = JSON.parse(fs.readFileSync('./db.json', 'utf-8'))
+
+  const newPerson = req.body
+
+  if (typeof newPerson.name !== 'string' || typeof newPerson.number !== 'string') {
+    res.status(400).json({ error: 'person properties missing or invalid' })
+  }
+
+  if (persons.some(person => person.name === newPerson.name)) {
+    res.status(409).json({ error: 'name exists in phone book' })
+  }
+
+  newPerson.id = uuid.v4()
+  persons.push(newPerson)
+
+  fs.writeFileSync('./db.json', JSON.stringify(persons))
+  res.sendStatus(200)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  let persons = JSON.parse(fs.readFileSync('./db.json', 'utf-8'))
+
+  persons = persons.filter(person => person.id !== req.params.id)
+
+  fs.writeFileSync('./db.json', JSON.stringify(persons))
+  res.sendStatus(204)
+})
+
+app.get('/info', (req, res) => {
+  const persons = JSON.parse(fs.readFileSync('./db.json', 'utf-8'))
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>\n${new Date().toString()}`)
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
